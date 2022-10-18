d6972425a8a2c8f660d22945926626c77a3056dc
oluwatos94 <fateemaaleeyu42@gmail.com>
2022-10-18 15:44:34 -0700

    efiko

--- 1-printf.c
+++ 1-printf.c
@@ -1,9 +1,11 @@
 #include "main.h"
+
 /**
  * _printf - prints and input into the standard output
  * @format: the format string
  * Return: number of bytes printed
  */
+
 int _printf(const char *format, ...)
 
 {
@@ -12,17 +14,16 @@ int _printf(const char *format, ...)
 	char *p, *start;
 
 	params_t params = PARAMS_INIT;
+
 	va_start(ap, format);
 
 	if (!format || (format[0] == '%' && !format[1]))/* checking for NULL char */
 		return (-1);
 	if (format[0] == '%' && format[1] == ' ' && !format[2])
 		return (-1);
-
 	for (p = (char *)format; *p; p++)
 	{
 		init_params(&params, ap);
-
 		if (*p != '%')/*checking for the % specifier*/
 		{
 			sum += _putchar(*p);
@@ -30,7 +31,6 @@ int _printf(const char *format, ...)
 		}
 		start = p;
 		p++;
-
 		while (get_flag(p, &params)) /* while char at p is flag character */
 		{
 			p++; /* next character */
@@ -47,6 +47,5 @@ int _printf(const char *format, ...)
 	}
 	_putchar(BUF_FLUSH);
 	va_end(ap);
-
 	return (sum);
 }
